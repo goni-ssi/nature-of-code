c741e3ed3e4b59205e526575537f0a9192847cd1
yungo1846 <yungo1468@naver.com>
2025-10-26 12:32:22 +0900

    feat: Pointing in the Direction of Motion

---
+++ src/sections/03.oscillation/3.3-pointing-in-the-direction-of-motion/index.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import { Vector } from "../../../common/utils/vector";
+import { Mover } from "./mover";
+import { Canvas, type CanvasProps } from "@/common/components/canvas";
+
+const PointingInTheDirectionOfMotion = () => {
+  const canvasRef = useRef<CanvasProps>(null);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const canvas = canvasRef.current;
+
+    const mover = new Mover({
+      canvas,
+      position: new Vector(0, 0),
+      velocity: new Vector(4, 4),
+    });
+
+    const animate = () => {
+      requestAnimationFrame(() => {
+        canvas.ctx.clearRect(0, 0, canvas.stageWidth, canvas.stageHeight);
+        mover.draw();
+        animate();
+      });
+    };
+
+    animate();
+  }, []);
+
+  return <Canvas ref={canvasRef} />;
+};
+
+export default PointingInTheDirectionOfMotion;
---
+++ src/sections/03.oscillation/3.3-pointing-in-the-direction-of-motion/mover.ts
@@ -0,0 +1,90 @@
+import type { CanvasProps } from "@/common/components/canvas";
+import { MousePointer } from "../../../common/class/mouse-pointer";
+import { Vector } from "../../../common/utils/vector";
+
+export class Mover {
+  public canvas: CanvasProps;
+  public position: Vector;
+  public velocity: Vector;
+  private mousePointer: MousePointer;
+  private angle: number;
+  private width: number;
+  private height: number;
+
+  constructor({
+    canvas,
+    position,
+    velocity,
+  }: {
+    canvas: CanvasProps;
+    position: Vector;
+    velocity: Vector;
+  }) {
+    this.canvas = canvas;
+    this.position = position;
+    this.velocity = velocity;
+    this.angle = 0;
+    this.width = 60;
+    this.height = 20;
+    this.mousePointer = new MousePointer({
+      x: this.canvas.ctx.canvas.offsetLeft,
+      y: this.canvas.ctx.canvas.offsetTop,
+    });
+  }
+
+  draw() {
+    const mousePosition = this.mousePointer.getPosition();
+    const attractor = new Vector(mousePosition.x, mousePosition.y);
+
+    this.canvas.ctx.save();
+    this.drawRect();
+    this.drawLine(attractor);
+    this.update(attractor);
+  }
+
+  drawRect() {
+    const centerX = this.position.x + this.width / 2;
+    const centerY = this.position.y + this.height / 2;
+
+    this.canvas.ctx.translate(centerX, centerY);
+    this.canvas.ctx.rotate(this.angle);
+    this.canvas.ctx.translate(-centerX, -centerY);
+
+    this.canvas.ctx.beginPath();
+    this.canvas.ctx.rect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
+    this.canvas.ctx.fillStyle = "green";
+    this.canvas.ctx.fill();
+    this.canvas.ctx.closePath();
+
+    this.canvas.ctx.restore();
+  }
+
+  drawLine(attractor: Vector) {
+    this.canvas.ctx.beginPath();
+    this.canvas.ctx.moveTo(
+      this.position.x + this.width / 2,
+      this.position.y + this.height / 2
+    );
+    this.canvas.ctx.lineTo(attractor.x, attractor.y);
+    this.canvas.ctx.strokeStyle = "red";
+    this.canvas.ctx.stroke();
+    this.canvas.ctx.closePath();
+  }
+
+  update(attractor: Vector) {
+    attractor.subtract(this.position);
+    attractor.normalize();
+    attractor.multiply(0.2);
+
+    this.angle = Math.atan2(attractor.y, attractor.x);
+
+    this.velocity.add(attractor);
+    this.velocity.limit(10);
+    this.position.add(this.velocity);
+  }
+}
